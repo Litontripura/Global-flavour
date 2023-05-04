e42bbd8f1322b8d42c3f1fa0c669f7f5a7ab320e
Litontripura <litontripura01@gmail.com>
2023-05-04 08:56:19 +0600

    i have done the registration page

--- src/components/Recipies/RecipiesCard.jsx
+++ src/components/Recipies/RecipiesCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RecipiesCard = ({ resi }) => {
   const {
@@ -10,25 +10,53 @@ const RecipiesCard = ({ resi }) => {
     is_favorite,
     photo_url,
   } = resi;
-// Each recipe info should contain the recipe name, ingredients (at least 5), cooking method, rating, and a Favorite button. Show at least 3 recipes. for each chef. If needed, you can have duplicate recipes for each chef
+
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const handleFavoriteClick = () => {
+    setIsFavorited(true);
+  };
+
   return (
-    <div className="relative text-left md:grid grid-cols-2 gap-3">
-        <img className="md:w-[500px] h-[250px] rounded w-full  " src={photo_url} alt="" />
+    <div className="relative text-left md:grid grid-cols-2 gap-3 mt-5">
+      <img
+        className="md:w-[500px] h-[250px] rounded w-full  "
+        src={photo_url}
+        alt=""
+      />
       <div className=" bg-orange-500 p-5 rounded">
         <h2 className="font-bold text-white ">{name}</h2>
-        
-     <div className="">
-      <h1 className="text-xl font-bold text-white">ingredients</h1>
-         {ingredients.map((ingre, index)=><p className="text-sm font-bold uppercase">{index+1}..{ingre}</p>)}
-     </div>
+
         <div className="">
-          <div className=""><span className="text-lg font-bold text-white">Cooking method</span> {cooking_method.length < 50 ? <p className="text-white">{cooking_method}</p> :
-                        <p className="text-white">{cooking_method.slice(0, 50)}...</p>
-                    }</div>
-         
+          <h1 className="text-xl font-bold text-white">ingredients</h1>
+          {ingredients.map((ingre, index) => (
+            <p className="text-sm font-bold uppercase">
+              {index + 1}..{ingre}
+            </p>
+          ))}
+        </div>
+        <div className="">
+          <div className="">
+            <span className="text-lg font-bold text-white">
+              Cooking method
+            </span>{" "}
+            {cooking_method.length < 50 ? (
+              <p className="text-white">{cooking_method}</p>
+            ) : (
+              <p className="text-white">{cooking_method.slice(0, 50)}...</p>
+            )}
+          </div>
         </div>
       </div>
-      <button className="absolute left-0 bottom-0 py-2 px-3 bg-orange-500 hover:bg-orange-700 text-white font-bold rounded-lg">Favorite</button>
+      <button
+  className={`absolute right-0 bottom-0 py-2 px-3 font-bold rounded-lg ${
+    isFavorited ? 'bg-green-500 hover:bg-green-700' : 'bg-slate-400 hover:bg-orange-700'
+  } text-white`}
+  onClick={handleFavoriteClick}
+  disabled={isFavorited}
+>
+  {isFavorited ? "Favorited" : "Favorite"}
+</button>
     </div>
   );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,62 +1,82 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './Register.css'
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthProvider';
+import { FaBeer, FaGithub, FaGoogle } from 'react-icons/fa';
 
 const Register = () => {
+  const [photo, setPhoto]=useState('')
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
   const {createUser}= useContext(AuthContext)
-          const handleRegister =(event)=>{
-            event.preventDefault()
-            const form = event.target;
-            const name = form.name.value;
-            const photo = form.photo.value;
-            const email = form.email.value;
-            const password = form.password.value;
-            const confirmPassword = form.confirmPassword.value;
-            console.log(name,photo, email, password, confirmPassword);
-            createUser(email,password)
-            .then(result=>{
-              console.log(result);
-              form.reset()
-            })
-            .catch(error=>{
-              console.log(error);
-            })
-            
-          }
+
+  const handleRegister = (event) => {
+    event.preventDefault()
+    const form = event.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirmPassword = form.confirmPassword.value;
+    setError('')
+    
+    console.log(name, photo, email, password, confirmPassword);
+    if (password.length < 6) {
+      setError('The password is less than 6 characters')
+      return;
+    }
+    if (name && email && password && photo) {
+      createUser(email, password)
+        .then((result) => {
+          console.log(result.user);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+  }
     return (
         <div className="flex justify-center items-center my-8">
-        <form onSubmit={handleRegister} className="register-bg p-8 rounded-lg shadow-lg w-2/5 register">
-          <h2 className="text-2xl font-medium mb-4">Register</h2>
-          
-            <div>
-              <label htmlFor="Name" className="block text-gray-700 font-medium mb-2">Name</label>
-              <input type="text" name='name' id="Name" className="border border-gray-400 p-2 w-full"  required />
-            </div>
-            <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 font-medium mb-2">Email</label>
-            <input type="email" id="email" name='email' className="border border-gray-400 p-2 w-full"  required />
-          </div>
-            <div>
-              <label htmlFor="photo" className="block text-gray-700 font-medium mb-2">Photo URL</label>
-              <input  type="text" id="photo" name='photo' className="border border-gray-400 p-2 w-full"  required />
-            </div>
-            
-          
-       
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700 font-medium mb-2">Password</label>
-            <input type="password" id="password" name='password' className="border border-gray-400 p-2 w-full"  required />
-          </div>
+       {photo && <img src={photo} alt="User" className="w-24 h-24 rounded-full mb-4" />}
+      <form onSubmit={handleRegister} className="register-bg p-8 rounded-lg shadow-lg w-2/5 register">
+        <h2 className="text-2xl font-medium mb-4">Register</h2>
+        <div>
+          <label htmlFor="Name" className="block text-gray-700 font-medium mb-2">Name</label>
+          <input type="text" name='name' onChange={(e) => setName(e.target.value)} id="Name" className="border border-gray-400 p-2 w-full" required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700 font-medium mb-2">Email</label>
+          <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} name='email' className="border border-gray-400 p-2 w-full" required />
+        </div>
+        <div>
+          <label htmlFor="photo" className="block text-gray-700 font-medium mb-2">Photo URL</label>
+          <input type="text" id="photo" onChange={(e) => setPhoto(e.target.value)} name='photo' className="border border-gray-400 p-2 w-full" required />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-gray-700 font-medium mb-2">Password</label>
+          <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} name='password' className="border border-gray-400 p-2 w-full" required />
+        </div>
           <div className="mb-4">
             <label htmlFor="confirmPassword" className="block text-gray-700 font-medium mb-2">Confirm Password</label>
             <input type="password" id="confirmPassword" name='confirmPassword' className="border border-gray-400 p-2 w-full"   required />
           </div>
-          <div className="flex items-center justify-between">
+          <div className="">
+          <p>Already have an account? <Link to='/login'>Please Login</Link> </p>
             <button type="submit" className="bg-green-500 hover:bg-green-200 text-white hover:text-black font-medium py-2 px-4 rounded">
-              Register
+            Please Register
             </button>
-             <p>Already have an account? <Link to='/login'>Please Login</Link> </p>
+            
+           <div className='mx-auto'>
+           <button className='flex justify-center items-center bg-green-500 hover:bg-green-200 text-white hover:text-black font-medium py-2 px-4 rounded my-2'>
+              <FaGoogle></FaGoogle> <p className='ms-2'>Sign with google</p>
+             </button>
+           </div>
+           <div>
+           <button className='flex  items-center bg-green-500 hover:bg-green-200 text-white hover:text-black font-medium py-2 px-4 rounded'><FaGithub></FaGithub><p className='ms-2'> Sign Up with Github</p></button>
+           </div>
           </div>
         </form>
       </div>
